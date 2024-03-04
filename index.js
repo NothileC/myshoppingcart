ef11ac997f3d0296ed248c49819838cf1cfa943e
NothileC <nothilecmhlanga@outlook.com>
2024-03-05 02:00:24 +0800

    updated js

--- index.js
+++ index.js
@@ -1,72 +1,30 @@
-const openShopping = document.querySelector(".openShopping");
-const closeShopping = document.querySelector(".closeShopping");
-const list = document.querySelector(".list");
-const listCard = document.querySelector(".listCard");
-const total = document.querySelector(".total");
-const body = document.querySelector("body");
-const quantity = document.querySelector(".quantity");
+//function to calculate total for item 1
+function calculateSubtotal(operator,quantityId){
+    calculateQuantity(operator,quantityId); 
+   
+    document.getElementById("lblUnitSubtotalProduct1").innerHTML = parseInt(document.getElementById("lblUnitPriceProduct1").innerHTML) * parseInt(document.getElementById("txtquantity1").value);
+
+    document.getElementById("lblUnitSubtotalProduct2").innerHTML = parseInt(document.getElementById("lblUnitPriceProduct2").innerHTML) * parseInt(document.getElementById("txtquantity2").value);
+
+    document.getElementById("lblUnitSubtotalProduct3").innerHTML = parseInt(document.getElementById("lblUnitPriceProduct3").innerHTML) * parseInt(document.getElementById("txtquantity3").value);
+
+    calculateGrandTotal();
 
 
-//Listeners for open shopping and close shopping
-openShopping.addEventListener("click", () =>{
-    body.classList.add("active")
-})
-closeShopping.addEventListener("click", () =>{
-    body.classList.remove("active")
-})
+}
 
-//product information and names
-let products =[{
-    id: 1,
-    name: "PRODUCT 1",
-    images: "1.PNG",
-    price: 750
-}, 
-{
-    id: 2,
-    name: "PRODUCT 2",
-    images: "2.PNG",
-    price: 300
-}, 
-{
-    id: 3,
-    name: "PRODUCT 3",
-    images: "3.PNG",
-    price: 500
-}, 
-{
-    id: 4,
-    name: "PRODUCT 4",
-    images: "4.PNG",
-    price: 800
-}, 
-{
-    id: 5,
-    name: "PRODUCT 5",
-    images: "5.PNG",
-    price: 450
-}, 
-{
-    id: 6,
-    name: "PRODUCT 6",
-    images: "6.PNG",
-    price: 1000
-}, 
-]
+function calculateQuantity(operator,quantityId){
+    if (operator == "+"){
+        document.getElementById(quantityId).value  = parseInt(document.getElementById(quantityId).value) + 1;
+    } 
+    else if (operator == "-"){
+        if(document.getElementById(quantityId).value > 0){
+            document.getElementById(quantityId).value  = parseInt(document.getElementById(quantityId).value) - 1;
+        }        
+    } 
+    
+}
 
-//empty array of the list cards
-let listCads = [];
-
-//function for shopping cart
-const initApp = () => {
-    products.forEach((value, key) => {
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("item");
-        newDiv.innerHTML = '
-        <img src ="img/$(value.images)">
-        <div class ="title">$(value.name)</div>
-        <div class ="price">$(value.price.tolocateString())</div>
-        <button onclick= "addToCard($(key))"Add To Card</button>
-        '
-    })
+function calculateGrandTotal(){
+    document.getElementById("text-grandtotal").value = parseInt( document.getElementById("lblUnitSubtotalProduct1").innerHTML ) + parseInt(document.getElementById("lblUnitSubtotalProduct2").innerHTML) +  parseInt( document.getElementById("lblUnitSubtotalProduct3").innerHTML )
 }
